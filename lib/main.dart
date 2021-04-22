d263cb29168df92a0b2ccca5810e7f9c839bfc3f
JaimeCuenca <jaime.cuenca.caro@gmail.com>
2021-04-22 13:09:49 +0200

    More widgets implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'mi APP Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -24,16 +24,48 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String texto = "Hola mundo de flutter";
     return Scaffold(
       backgroundColor: Colors.grey,
+
       appBar: AppBar(
-          backgroundColor: Colors.amber),
-      body:
-      Center(
-          child: Text("Hola Mundo de Flutter", style: TextStyle(
-              fontSize: 20,
-              color: Colors.amberAccent))
+          backgroundColor: Colors.amber,
+          title: Text('Primera App'),
+          actions: [
+            IconButton(icon: Icon(Icons.add), onPressed: __add )
+          ],
       ),
+
+      floatingActionButton:
+        FloatingActionButton(
+            backgroundColor: Colors.amber,
+            child:
+              Icon(Icons.ac_unit, color: Colors.black),
+            onPressed: __mensaje,
+          ),
+      drawer: Drawer(),
+      endDrawer: Drawer(),
+
+
+      body:
+        Column(
+
+            children: [
+              Image.network("https://media1.tenor.com/images/d35679a70d0b0ea47d541475105285a3/tenor.gif?itemid=13024140",
+              height: 300,),
+
+              Text(texto, style: TextStyle(
+                fontSize: 20,
+                color: Colors.amberAccent))]
+        ),
     );
+  }
+
+  void __mensaje() {
+    print("Mensaje desde floating action button");
+  }
+
+  void __add() {
+    print("Mensaje desde AppBar");
   }
 }
